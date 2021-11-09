45105293a9ea22b6a119a7f3b412e435a32ba527
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2021-11-09 13:16:30 +0100

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A simple client!");
+
+            TcpClient clientSocket = new TcpClient("localhost", 8000);
+            var writer = new StreamWriter(clientSocket.GetStream());
+            var reader = new StreamReader(clientSocket.GetStream());
+
+            Console.WriteLine($"{reader.ReadLine()}");
+            Console.WriteLine($"{reader.ReadLine()}");
+
+            string input = null;
+            while((input=Console.ReadLine())!="quit")
+            {
+                writer.WriteLine(input);
+                writer.Flush();
+            }
+            writer.WriteLine("quit");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A simple server running...\n");
+
+            TcpListener serverListener = new TcpListener(IPAddress.Loopback, 8000);
+            serverListener.Start(5);
+
+            while (true)
+            {
+                TcpClient clientSocket = serverListener.AcceptTcpClient();
+                new Task(() =>
+               {
+                   try
+                   {
+                       var writer = new StreamWriter(clientSocket.GetStream());
+                       var reader = new StreamReader(clientSocket.GetStream());
+
+                       writer.WriteLine("Welcome to my server!");
+                       writer.WriteLine("Please enter your commands...");
+                       writer.Flush();
+
+                       string message;
+                       do
+                       {
+                           message = reader.ReadLine();
+                           Console.WriteLine($"received: {message}");
+                       } while (message != "quit");
+                   }
+                   catch (Exception e)
+                   {
+                       Console.WriteLine($"error occured {e.Message}");
+                   }
+               }).Start();
+            }
+        }
+    }
+}
